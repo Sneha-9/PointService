9874ae3d57bbd785c356dc355e64882156008fab
Sneha <sneha@Nikhils-MacBook-Air.local>
2026-04-04 15:29:41 +0530

    test code bug fixed

--- src/test/resources/test-init.sql
+++ src/test/resources/test-init.sql
@@ -8,9 +8,3 @@ CREATE TABLE points
     updatedAt        TIMESTAMP WITHOUT TIME ZONE       NOT NULL
 );
 
-CREATE TABLE points
-(id               VARCHAR(255) PRIMARY KEY,recordid         VARCHAR(255),recordtype       VARCHAR(255),
-    aggregatedpoints numeric,
-    createdAt        TIMESTAMP WITHOUT TIME ZONE       NOT NULL,
-    updatedAt        TIMESTAMP WITHOUT TIME ZONE       NOT NULL
-);
