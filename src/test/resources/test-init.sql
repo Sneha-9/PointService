65811023b6d34f34fb101c6957410e44de9b8514
Sneha <sneha@Nikhils-MacBook-Air.local>
2026-04-04 15:21:49 +0530

    deployment code added

--- src/test/resources/test-init.sql
+++ src/test/resources/test-init.sql
@@ -7,3 +7,10 @@ CREATE TABLE points
     createdAt        TIMESTAMP WITHOUT TIME ZONE       NOT NULL,
     updatedAt        TIMESTAMP WITHOUT TIME ZONE       NOT NULL
 );
+
+CREATE TABLE points
+(id               VARCHAR(255) PRIMARY KEY,recordid         VARCHAR(255),recordtype       VARCHAR(255),
+    aggregatedpoints numeric,
+    createdAt        TIMESTAMP WITHOUT TIME ZONE       NOT NULL,
+    updatedAt        TIMESTAMP WITHOUT TIME ZONE       NOT NULL
+);
